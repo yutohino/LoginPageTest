80089c0cd658b715e73233e0fe67a3180d192466
yutohino <tanegashimav48273@gmail.com>
2020-07-25 21:31:39 +0900

    DataBindingの導入,EditTextの文字入力イベントを取得する機能を実装

--- app/src/main/java/com/example/loginpagetest/MainActivity.kt
+++ app/src/main/java/com/example/loginpagetest/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.loginpagetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
+import com.example.loginpagetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mBinding: ActivityMainBinding
+    val addressTextWatcher = object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {}
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            log()
+        }
+    }
+
+    val passTextWatcher = object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {}
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            log()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mBinding.edAddress.addTextChangedListener(addressTextWatcher)
+
+        mBinding.edPass.addTextChangedListener(passTextWatcher)
+    }
+
+    var int = 0
+    fun log() {
+        int++
+        println("入力した回数：$int")
     }
 }
