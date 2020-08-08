54f3961e8d29183d84b74be16904896aa7ba3a90
Yuto Hino <tanegashimav48273@gmail.com>
2020-08-08 22:51:01 +0900

    ユーザー名の欄をリアクティブにユーザー名検索できるように実装

--- app/src/main/java/com/example/loginpagetest/MainActivity.kt
+++ app/src/main/java/com/example/loginpagetest/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.loginpagetest
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -8,10 +10,25 @@ import android.util.Log
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.example.loginpagetest.databinding.ActivityMainBinding
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    val KEY_PREFS = "KEY_PREFS"
+    val KEY_STRING1 = "KEY_STRING1"
+    val KEY_STRING2 = "KEY_STRING2"
+    val KEY_STRING3 = "KEY_STRING3"
+    val KEY_STRING4 = "KEY_STRING4"
+
     lateinit var mBinding: ActivityMainBinding
-    val addressTextWatcher = object : TextWatcher {
+    val subject = PublishSubject.create<CharSequence>()
+    val compositeDisposable = CompositeDisposable()
+
+    val userTextWatcher = object : TextWatcher {
         override fun afterTextChanged(p0: Editable?) {}
 
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -19,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         override fun onTextChanged(c: CharSequence?, p1: Int, p2: Int, p3: Int) {
             c?.let {
                 observeOnUser(it)
-                setClickableBtn()
+                subject.onNext(it.toString())
             }
         }
     }
@@ -33,6 +50,7 @@ class MainActivity : AppCompatActivity() {
             c?.let {
                 observeOnPass(it)
                 setClickableBtn()
+                subject.onNext(it)
             }
         }
     }
@@ -42,16 +60,48 @@ class MainActivity : AppCompatActivity() {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
+    lateinit var prefs: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
+    var userList = ArrayList<String>()
     override fun onResume() {
         super.onResume()
+        val userDisposable = subject.distinctUntilChanged()
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+            searchUserSharedPref(it)
+        }
+        compositeDisposable.add(userDisposable)
 
-        mBinding.edUser.addTextChangedListener(addressTextWatcher)
+        mBinding.edUser.addTextChangedListener(userTextWatcher)
 
         mBinding.edPass.addTextChangedListener(passTextWatcher)
 
         mBinding.button.setOnClickListener {
 
         }
+
+        prefs = getSharedPreferences(KEY_PREFS, Context.MODE_PRIVATE)
+        editor = prefs.edit()
+        mBinding.buttonAdd.setOnClickListener {
+            editor.putString(KEY_STRING1, "ひの")
+            editor.putString(KEY_STRING2, "ゆうと")
+            editor.putString(KEY_STRING3, "yuto")
+            editor.putString(KEY_STRING4, "hino")
+            editor.commit()
+
+            addUserList()
+        }
+
+        addUserList()
+    }
+
+    fun addUserList() {
+        prefs.getString(KEY_STRING1, null)?.let { userList.add(it) }
+        prefs.getString(KEY_STRING2, null)?.let { userList.add(it) }
+        prefs.getString(KEY_STRING3, null)?.let { userList.add(it) }
+        prefs.getString(KEY_STRING4, null)?.let { userList.add(it) }
     }
 
     fun observeOnUser(c: CharSequence) {
@@ -78,5 +128,29 @@ class MainActivity : AppCompatActivity() {
             mBinding.button.isClickable = false
             mBinding.button.background = resources.getDrawable(R.drawable.bg_button_disable)
         }
+    }
+
+    fun searchUserSharedPref(c: CharSequence) {
+        for (user in userList) {
+            if (c.toString() == user) {
+                mBinding.textAlert.text = "そのユーザー名はすでに使われています"
+                mBinding.button.isClickable = false
+                mBinding.button.background = resources.getDrawable(R.drawable.bg_button_disable)
+                return
+            }
+            if (c.toString() != user && mBinding.textAlert.text.isNotEmpty()) {
+                mBinding.textAlert.text = ""
+                setClickableBtn()
+            }
+        }
+    }
+
+    fun searchPassSharedPref(c: CharSequence) {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 }
